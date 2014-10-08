3f8c904069361daf0173c33d2d61ffd47f03958b
RogerMendez <Roger.Mendez.R@gmail.com>
2014-10-08 01:56:02 -0400

    heroku init

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ahiteveo.wsgi --log-file -
--- ahiteveo/settings.py
+++ ahiteveo/settings.py
@@ -123,3 +123,24 @@ EMAIL_USE_TLS = True
 #from django.contrib.auth.models import User
 #AUTH_USER_MODEL = User
 AUTH_PROFILE_MODULE = 'users.Perfil'
+
+
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
