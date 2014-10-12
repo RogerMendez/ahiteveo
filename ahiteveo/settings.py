9e6fdb6307a4ee9598fa35d9b4a6e9d1f0db8a15
RogerMendez <Roger.Mendez.R@gmail.com>
2014-10-11 22:52:57 -0400

    heroku 4

--- ahiteveo/settings.py
+++ ahiteveo/settings.py
@@ -126,14 +126,14 @@ AUTH_PROFILE_MODULE = 'users.Perfil'
 
 
 
-#import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
-#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-#ALLOWED_HOSTS = ['*']
-#import os
-#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-#STATIC_ROOT = 'staticfiles'
-#STATIC_URL = '/static/'
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ALLOWED_HOSTS = ['*']
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
